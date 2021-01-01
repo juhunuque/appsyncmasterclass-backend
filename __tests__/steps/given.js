c428b2b1f2ffa16021839151b0e90957e104bbde
Julio Núñez Quesada <juhunuque@gmail.com>
2020-12-31 20:25:55 -0600

    Progress - Chapter 4: Add acceptance test for getMyProfile query

--- __tests__/steps/given.js
+++ __tests__/steps/given.js
@@ -1,5 +1,7 @@
 const chance = require('chance').Chance()
 const velocityUtil = require('amplify-appsync-simulator/lib/velocity/util')
+const AWS = require('aws-sdk')
+require('dotenv').config()
 
 const a_random_user = () => {
   const firstName = chance.first({nationality: 'en'})
@@ -32,7 +34,53 @@ const an_appsync_context = (identity, args) => {
 
 }
 
+const an_authenticated_user = async () => {
+  const cognito = new AWS.CognitoIdentityServiceProvider()
+
+  const userPoolId = process.env.COGNITO_USER_POOL_ID
+  const clientId = process.env.WEB_COGNITO_USER_POOL_CLIENT_ID
+
+  const {name, email, password} = a_random_user()
+  const signUpResp = await cognito.signUp({
+    ClientId: clientId,
+    Username: email,
+    Password: password,
+    UserAttributes: [
+      { Name: 'name', Value: name }
+    ]
+  }).promise()
+  const username = signUpResp.UserSub
+  console.log(`[${email}] - user has signed up [${username}]`)
+
+  await cognito.adminConfirmSignUp({
+    UserPoolId: userPoolId,
+    Username: username
+  }).promise()
+
+  console.log(`[${email}] - confirmed sign up`)
+
+  const auth = await cognito.initiateAuth({
+    AuthFlow: 'USER_PASSWORD_AUTH',
+    ClientId: clientId,
+    AuthParameters: {
+      USERNAME: username,
+      PASSWORD: password
+    }
+  }).promise()
+
+  console.log(`[${email}] - signed in`)
+
+  return {
+    username,
+    name,
+    email,
+    idToken: auth.AuthenticationResult.idToken,
+    accessToken: auth.AuthenticationResult.AccessToken
+  }
+}
+
 module.exports = {
   a_random_user,
-  an_appsync_context
+  an_appsync_context,
+  an_authenticated_user
 }
--- __tests__/test_cases/unit/Query.getMyProfile.request.tests.js
+++ __tests__/test_cases/unit/Query.getMyProfile.request.tests.js
@@ -1,6 +1,5 @@
 const given = require('../../steps/given')
 const when = require('../../steps/when')
-const then = require('../../steps/then')
 const chance = require('chance').Chance()
 const path = require('path')
 
