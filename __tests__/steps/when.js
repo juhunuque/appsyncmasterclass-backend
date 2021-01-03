6097260c858a924ecb47b52a89bd65579f234677
Julio Núñez Quesada <juhunuque@gmail.com>
2021-01-03 14:15:04 -0600

    Progress - Chapter 4: Add acceptance test for tweet mutation

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -169,6 +169,29 @@ const a_user_calls_getImageUploadUrl = async (user, extension, contentType) => {
   return url
 }
 
+const a_user_calls_tweet = async (user, text) => {
+  const tweet = `mutation tweet($text: String!) {
+    tweet(text: $text) {
+      id
+      createdAt
+      text
+      replies
+      likes
+      retweets
+    }
+  }`
+  const variables = {
+    text
+  }
+
+  const data = await GraphQL(process.env.API_URL, tweet, variables, user.accessToken)
+  const newTweet = data.tweet
+
+  console.log(`[${user.username}] - posted new tweet`)
+
+  return newTweet
+}
+
 const we_invoke_tweet = async (username, text) => {
   const handler = require('../../functions/tweet').handler
 
@@ -193,5 +216,6 @@ module.exports = {
   a_user_calls_editMyProfile,
   we_invoke_getImageUploadUrl,
   a_user_calls_getImageUploadUrl,
-  we_invoke_tweet
+  we_invoke_tweet,
+  a_user_calls_tweet
 }
