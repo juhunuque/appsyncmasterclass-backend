fe7b36f89961ffbb52be8cb24a7ab68daf0fa8f8
Julio Núñez Quesada <juhunuque@gmail.com>
2021-01-14 16:34:24 -0600

    Progress - Chapter 4: Add integration tests for retweet mutation

--- __tests__/steps/then.js
+++ __tests__/steps/then.js
@@ -1,26 +1,134 @@
 require('dotenv').config()
+const _ = require('lodash')
 const AWS = require('aws-sdk')
 const http = require('axios')
 const fs = require('fs')
 
 const user_exists_in_UsersTable = async (id) => {
   const DynamoDB = new AWS.DynamoDB.DocumentClient()
-  const tableName = process.env.USERS_TABLE
 
-  console.log(`looking for user [${id}] in table [${tableName}]`)
+  console.log(`looking for user [${id}] in table [${process.env.USERS_TABLE}]`)
   const resp = await DynamoDB.get({
-    TableName: tableName,
+    TableName: process.env.USERS_TABLE,
     Key: {
       id
     }
   }).promise()
 
   expect(resp.Item).toBeTruthy()
+
   return resp.Item
 }
 
-const user_can_upload_image_to_url = async (url, filePath, contentType) => {
-  const data = fs.readFileSync(filePath)
+const tweetsCount_is_updated_in_UsersTable = async (id, newCount) => {
+  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+
+  console.log(`looking for user [${id}] in table [${process.env.USERS_TABLE}]`)
+  const resp = await DynamoDB.get({
+    TableName: process.env.USERS_TABLE,
+    Key: {
+      id
+    }
+  }).promise()
+
+  expect(resp.Item).toBeTruthy()
+  expect(resp.Item.tweetsCount).toEqual(newCount)
+
+  return resp.Item
+}
+
+const tweet_exists_in_TweetsTable = async (id) => {
+  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+
+  console.log(`looking for tweet [${id}] in table [${process.env.TWEETS_TABLE}]`)
+  const resp = await DynamoDB.get({
+    TableName: process.env.TWEETS_TABLE,
+    Key: {
+      id
+    }
+  }).promise()
+
+  expect(resp.Item).toBeTruthy()
+
+  return resp.Item
+}
+
+const retweet_exists_in_TweetsTable = async (userId, tweetId) => {
+  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+
+  console.log(`looking for retweet of [${tweetId}] in table [${process.env.TWEETS_TABLE}]`)
+  const resp = await DynamoDB.query({
+    TableName: process.env.TWEETS_TABLE,
+    IndexName: 'retweetsByCreator',
+    KeyConditionExpression: 'creator = :creator AND retweetOf = :tweetId',
+    ExpressionAttributeValues: {
+      ':creator': userId,
+      ':tweetId': tweetId
+    },
+    Limit: 1
+  }).promise()
+
+  const retweet = _.get(resp, 'Items.0')
+
+  expect(retweet).toBeTruthy()
+
+  return retweet
+}
+
+const retweet_exists_in_RetweetsTable = async (userId, tweetId) => {
+  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+
+  console.log(`looking for retweet of [${tweetId}] for user [${userId}] in table [${process.env.RETWEETS_TABLE}]`)
+  const resp = await DynamoDB.get({
+    TableName: process.env.RETWEETS_TABLE,
+    Key: {
+      userId,
+      tweetId
+    }
+  }).promise()
+
+  expect(resp.Item).toBeTruthy()
+
+  return resp.Item
+}
+
+const tweet_exists_in_TimelinesTable = async (userId, tweetId) => {
+  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+
+  console.log(`looking for tweet [${tweetId}] for user [${userId}] in table [${process.env.TIMELINES_TABLE}]`)
+  const resp = await DynamoDB.get({
+    TableName: process.env.TIMELINES_TABLE,
+    Key: {
+      userId,
+      tweetId
+    }
+  }).promise()
+
+  expect(resp.Item).toBeTruthy()
+
+  return resp.Item
+}
+
+const there_are_N_tweets_in_TimelinesTable = async (userId, n) => {
+  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+
+  console.log(`looking for [${n}] tweets for user [${userId}] in table [${process.env.TIMELINES_TABLE}]`)
+  const resp = await DynamoDB.query({
+    TableName: process.env.TIMELINES_TABLE,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': userId
+    },
+    ScanIndexForward: false
+  }).promise()
+
+  expect(resp.Items).toHaveLength(n)
+
+  return resp.Items
+}
+
+const user_can_upload_image_to_url = async (url, filepath, contentType) => {
+  const data = fs.readFileSync(filepath)
   await http({
     method: 'put',
     url,
@@ -30,70 +138,25 @@ const user_can_upload_image_to_url = async (url, filePath, contentType) => {
     data
   })
 
-  console.log(`uploaded image to`, url)
+  console.log('uploaded image to', url)
 }
 
 const user_can_download_image_from = async (url) => {
-  const resp = await(http(url))
-  console.log(`downloaded image from`, url)
+  const resp = await http(url)
+
+  console.log('downloaded image from', url)
+
   return resp.data
-}
-
-const tweet_exists_in_TweetsTable = async (id) => {
-  const DynamoDB = new AWS.DynamoDB.DocumentClient()
-  const {TWEETS_TABLE} = process.env
-
-  console.log(`looking for tweet [${id}] in table [${TWEETS_TABLE}]`)
-  const resp = await DynamoDB.get({
-    TableName: TWEETS_TABLE,
-    Key: {
-      id
-    }
-  }).promise()
-
-  expect(resp.Item).toBeTruthy()
-  return resp.Item
-}
-
-const tweet_exists_in_TimelinesTable = async (userId, tweetId) => {
-  const DynamoDB = new AWS.DynamoDB.DocumentClient()
-  const {TIMELINES_TABLE} = process.env
-
-  console.log(`looking for tweet [${tweetId}] for user [${userId}] in table [${TIMELINES_TABLE}]`)
-  const resp = await DynamoDB.get({
-    TableName: TIMELINES_TABLE,
-    Key: {
-      userId,
-      tweetId
-    }
-  }).promise()
-
-  expect(resp.Item).toBeTruthy()
-  return resp.Item
-}
-
-const tweetsCount_is_updated_in_UsersTable = async (id, newCount) => {
-  const DynamoDB = new AWS.DynamoDB.DocumentClient()
-  const tableName = process.env.USERS_TABLE
-
-  console.log(`looking for user [${id}] in table [${tableName}]`)
-  const resp = await DynamoDB.get({
-    TableName: tableName,
-    Key: {
-      id
-    }
-  }).promise()
-
-  expect(resp.Item).toBeTruthy()
-  expect(resp.Item.tweetsCount).toEqual(newCount)
-  return resp.Item
 }
 
 module.exports = {
   user_exists_in_UsersTable,
-  user_can_upload_image_to_url,
-  user_can_download_image_from,
+  tweetsCount_is_updated_in_UsersTable,
   tweet_exists_in_TweetsTable,
   tweet_exists_in_TimelinesTable,
-  tweetsCount_is_updated_in_UsersTable
+  retweet_exists_in_TweetsTable,
+  retweet_exists_in_RetweetsTable,
+  there_are_N_tweets_in_TimelinesTable,
+  user_can_upload_image_to_url,
+  user_can_download_image_from
 }
